280fb21536bc6d1073527266cdb723515e0e3fce
George Constantinou <g.constantinou@qobo.biz>
2016-03-28 18:07:59 +0300

    add trait to handle module configuration settings (task #1610)

---
+++ src/ConfigurationTrait.php
@@ -0,0 +1,90 @@
+<?php
+namespace CsvMigrations;
+
+use Cake\Core\Configure;
+
+trait ConfigurationTrait
+{
+    /**
+     * Table/module configuration
+     *
+     * @var array
+     */
+    protected $_config = [];
+
+    /**
+     * Config filename
+     *
+     * @var string
+     */
+    protected $_filename = 'config';
+
+    /**
+     * Config file extension
+     *
+     * @var string
+     */
+    protected $_extension = 'ini';
+
+    /**
+     * Searchable flag
+     *
+     * @var boolean
+     */
+    protected $_searchable = true;
+
+    /**
+     * Module alias
+     *
+     * @var string
+     */
+    protected $_moduleAlias;
+
+    /**
+     * Method that sets table configuration.
+     *
+     * @return void
+     */
+    protected function _setConfiguration()
+    {
+        $path = Configure::read('CsvMigrations.migrations.path');
+        $path .= $this->alias() . DS . $this->_filename . '.' . $this->_extension;
+        if (file_exists($path)) {
+            $this->_config = parse_ini_file($path, true);
+        }
+    }
+
+    /**
+     * Returns the searchable flag or sets a new one
+     *
+     * @param  bool|null $searchable sets module as (not) searchable
+     * @return bool
+     */
+    public function searchable(bool $searchable = null)
+    {
+        if (!is_null($searchable)) {
+            $this->_searchable = (bool)$searchable;
+        }
+
+        return $this->_searchable;
+    }
+
+    /**
+     * Returns the module alias or sets a new one
+     *
+     * @param  string|null $alias sets a new name to be used as module alias
+     * @return string
+     */
+    public function moduleAlias($alias = null)
+    {
+        if (!is_null($alias)) {
+            $this->_moduleAlias = $alias;
+        }
+
+        if (is_null($this->_moduleAlias)) {
+            $this->_moduleAlias = $this->alias();
+        }
+
+        return $this->_moduleAlias;
+    }
+}

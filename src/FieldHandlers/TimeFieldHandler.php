1b29d9585446d6fa68167b8ece468287b0158ad8
George Constantinou <g.constantinou@qobo.biz>
2016-04-11 16:43:57 +0300

    add field handlers for date, time and datetime fields (task #1750)

---
+++ src/FieldHandlers/TimeFieldHandler.php
@@ -0,0 +1,35 @@
+<?php
+namespace CsvMigrations\FieldHandlers;
+
+use App\View\AppView;
+use CsvMigrations\FieldHandlers\BaseFieldHandler;
+
+class TimeFieldHandler extends BaseFieldHandler
+{
+    const FIELD_TYPE = 'timepicker';
+
+    /**
+     * Method responsible for rendering field's input.
+     *
+     * @param  mixed  $table   name or instance of the Table
+     * @param  string $field   field name
+     * @param  string $data    field data
+     * @param  array  $options field options
+     * @return string          field input
+     */
+    public function renderInput($table, $field, $data = '', array $options = [])
+    {
+        // load AppView
+        $cakeView = new AppView();
+
+        return $cakeView->element('QoboAdminPanel.datepicker', [
+            'options' => [
+                'fieldName' => $field,
+                'type' => static::FIELD_TYPE,
+                'label' => true,
+                'required' => (bool)$options['fieldDefinitions']['required'],
+                'value' => $data
+            ]
+        ]);
+    }
+}

4ce31b18de107311a6428d09ba1fe9f9050521a4
Antonis Flangofas <a.flangofas@qobo.biz>
2016-05-18 17:49:42 +0300

    On edit show a link and unlink option (task #1804)

--- src/FieldHandlers/FileFieldHandler.php
+++ src/FieldHandlers/FileFieldHandler.php
@@ -13,15 +13,45 @@ class FileFieldHandler extends BaseFieldHandler
      */
     const WRAPPER = '<div class="form-group">%s%s</div>';
 
+    const DIV = '<div>%s&nbsp;%s</div>';
     /**
      * {@inheritDoc}
      */
     public function renderInput($table, $field, $data = '', array $options = [])
     {
         $cakeView = new AppView();
-        $uploadField = $cakeView->Form->file('UploadDocuments.file');
-        $label = $cakeView->Form->label($field);
-        $result = sprintf(self::WRAPPER, $label, $uploadField);
+        if (!$data) {
+            $uploadField = $cakeView->Form->file('UploadDocuments.file');
+            $label = $cakeView->Form->label($field);
+            $result = sprintf(self::WRAPPER, $label, $uploadField);
+        } else {
+            $label = $cakeView->Form->label($field);
+            $cakeView->loadHelper('Burzum/FileStorage.Storage', [
+                'pathBuilderOptions' => [
+                    'pathPrefix' => '/uploads'
+                ]
+            ]);
+            $entity = $table->uploaddocuments->find()
+                ->where(['id' => $data])
+                ->first();
+            $url = $cakeView->Storage->url($entity);
+            $viewLink = $cakeView->Html->link(
+                __d('CsvMigrations', 'View File'),
+                $cakeView->Url->build($url),
+                ['target' => '_blank']
+            );
+            $removeLink = $cakeView->Form->postLink(
+                '',
+                ['action' => 'deleteUpload', $data],
+                [
+                    'confirm' => __d('CsvMigration', 'Are you sure you want to delete?'),
+                    'title' => __d('CsvMigration', 'Delete'),
+                    'class' => 'btn btn-default glyphicon glyphicon-trash'
+                ]
+            );
+            $div = sprintf(self::DIV, $viewLink, $removeLink);
+            $result = sprintf(self::WRAPPER, $label, $div);
+        }
 
         return $result;
     }

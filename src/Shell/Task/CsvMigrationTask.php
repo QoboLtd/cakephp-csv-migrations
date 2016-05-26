6e0a162c8c8cd270da7a9e4ec2afb7e3ac4a5df5
George Constantinou <g.constantinou@qobo.biz>
2016-05-26 18:50:23 +0300

    task for extending cakephp bake functionality to bake csv migrations (task #2071)

---
+++ src/Shell/Task/CsvMigrationTask.php
@@ -0,0 +1,98 @@
+<?php
+namespace CsvMigrations\Shell\Task;
+
+use Cake\Utility\Inflector;
+use Migrations\Shell\Task\MigrationTask;
+use Phinx\Util\Util;
+
+/**
+ * CsvMigrations baking migration task, used to extend CakePHP's bake functionality.
+ */
+class CsvMigrationTask extends MigrationTask
+{
+    /**
+     * Timestamp
+     * @var string
+     */
+    private $__timestamp;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function main($name = null)
+    {
+        $this->__timestamp = Util::getCurrentTimestamp();
+
+        parent::main($name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function name()
+    {
+        return 'CSV Migration';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function fileName($name)
+    {
+        $name = $this->getMigrationName($name);
+
+        return $this->__timestamp . '_' . Inflector::camelize($name) . $this->_getUniqueName() . '.php';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function template()
+    {
+        return 'CsvMigrations.csv_migration';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function templateData()
+    {
+        $className = $this->BakeTemplate->viewVars['name'];
+        $action = $this->detectAction($className);
+
+        if (empty($action)) {
+            $table = $className;
+            $action = 'create_table';
+        } else {
+            list($action, $table) = $action;
+        }
+        $table = Inflector::tableize($table);
+
+        $name = Inflector::camelize($className) . $this->_getUniqueName();
+
+        return [
+            'action' => $action,
+            'table' => $table,
+            'name' => $name
+        ];
+    }
+
+    /**
+     * Generate and return unique alphabetic string.
+     *
+     * @return string
+     */
+    protected function _getUniqueName($timestamp = null)
+    {
+        $alphas = range('A', 'Z');
+        $numbers = str_split($this->__timestamp);
+
+        array_walk($numbers, function (&$number, $k, $alphas) {
+            if (array_key_exists($number, $alphas)) {
+                $number = $alphas[$number];
+            }
+        }, $alphas);
+
+        return implode('', $numbers);
+    }
+}

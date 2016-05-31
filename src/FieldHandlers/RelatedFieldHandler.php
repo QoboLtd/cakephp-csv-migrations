24f11f37567324ce92cda0e436980a03af808ab4
George Constantinou <g.constantinou@qobo.biz>
2016-05-31 17:00:49 +0300

    move logic into a Trait (task #2070)

--- src/FieldHandlers/RelatedFieldHandler.php
+++ src/FieldHandlers/RelatedFieldHandler.php
@@ -11,16 +11,12 @@ use CsvMigrations\FieldHandlers\BaseFieldHandler;
 class RelatedFieldHandler extends BaseFieldHandler
 {
     use IdGeneratorTrait;
+    use RelatedFieldTrait;
 
     /**
      * Field type
      */
     const FIELD_TYPE = 'uuid';
-
-    /**
-     * Field type match pattern
-     */
-    const FIELD_TYPE_PATTERN = '/related\((.*?)\)/';
 
     /**
      * Action name for html link
@@ -156,106 +152,5 @@ class RelatedFieldHandler extends BaseFieldHandler
         );
 
         return $dbFields;
-    }
-
-    /**
-     * Method that extracts list name from field type definition.
-     *
-     * @param  string $type field type
-     * @return string       list name
-     */
-    protected function _getRelatedName($type)
-    {
-        $result = preg_replace(static::FIELD_TYPE_PATTERN, '$1', $type);
-
-        return $result;
-    }
-
-    /**
-     * Get related model's parent model properties.
-     *
-     * @param  array $table related model properties
-     * @return void
-     */
-    protected function _getRelatedParentProperties($relatedProperties)
-    {
-        $parentTable = TableRegistry::get($relatedProperties['config']['parent']['module']);
-        $foreignKey = $this->_getForeignKey($parentTable, $relatedProperties['controller']);
-
-        return $this->_getRelatedProperties($parentTable, $relatedProperties['entity']->{$foreignKey});
-    }
-
-    /**
-     * Get related model's properties.
-     *
-     * @param  mixed $table related table instance or name
-     * @param  sting $data  query parameter value
-     * @return void
-     */
-    protected function _getRelatedProperties($table, $data)
-    {
-        if (!is_object($table)) {
-            $tableName = $table;
-            $table = TableRegistry::get($tableName);
-        } else {
-            $tableName = $table->registryAlias();
-        }
-
-        $result['id'] = $data;
-        $result['config'] = $table->getConfig();
-        // get associated entity record
-        $result['entity'] = $this->_getAssociatedRecord($table, $data);
-        // get related table's displayField value
-        $result['dispFieldVal'] = !empty($result['entity']->{$table->displayField()})
-            ? $result['entity']->{$table->displayField()}
-            : null
-        ;
-        // get plugin and controller names
-        list($result['plugin'], $result['controller']) = pluginSplit($tableName);
-        // remove vendor from plugin name
-        if (!is_null($result['plugin'])) {
-            $pos = strpos($result['plugin'], '/');
-            if ($pos !== false) {
-                $result['plugin'] = substr($result['plugin'], $pos + 1);
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * Get parent model association's foreign key.
-     *
-     * @param  \Cake\ORM\Table $table          Table instance
-     * @param  string          $controllerName Controller name
-     * @return string
-     */
-    protected function _getForeignKey(Table $table, $controllerName)
-    {
-        $result = null;
-        foreach ($table->associations() as $association) {
-            if ($controllerName === $association->className()) {
-                $result = $association->foreignKey();
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * Retrieve and return associated record Entity, by primary key value.
-     *
-     * @param  \Cake\ORM\Table $table Table instance
-     * @param  string          $value Primary key value
-     * @return object
-     */
-    protected function _getAssociatedRecord(Table $table, $value)
-    {
-        $query = $table->find('all', [
-            'conditions' => [$table->primaryKey() => $value],
-            'limit' => 1
-        ]);
-
-        return $query->first();
     }
 }

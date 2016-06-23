23ba7527e553827cb99c6a3898d2ce7cb6b9db44
George Constantinou <g.constantinou@qobo.biz>
2016-06-23 09:42:07 +0300

    Unit test for Migration Trait (task #2208)

---
+++ tests/TestCase/MigrationTraitTest.php
@@ -0,0 +1,125 @@
+<?php
+namespace CsvMigrations\Test\TestCase;
+
+use Cake\Core\Configure;
+use CsvMigrations\MigrationTrait;
+use PHPUnit_Framework_TestCase;
+
+class MigrationTraitTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->mock = $this->getMockForTrait(MigrationTrait::class);
+
+        $dir = dirname(__DIR__) . DS . 'data' . DS . 'CsvMigrations' . DS . 'migrations' . DS;
+        Configure::write('CsvMigrations.migrations.path', $dir);
+    }
+
+    /**
+     * @dataProvider csvProvider
+     */
+    public function testGetFieldsDefinitions($name, $expected)
+    {
+        $this->assertEquals($expected, $this->mock->getFieldsDefinitions($name));
+    }
+
+    public function csvProvider()
+    {
+        return [
+            [
+                'WrongName',
+                []
+            ],
+            [
+                'Foo',
+                [
+                    'id' => [
+                        'name' => 'id',
+                        'type' => 'uuid',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'description' => [
+                        'name' => 'description',
+                        'type' => 'text',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'name' => [
+                        'name' => 'name',
+                        'type' => 'string',
+                        'required' => '1',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'status' => [
+                        'name' => 'status',
+                        'type' => 'list(foo_statuses)',
+                        'required' => '1',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'type' => [
+                        'name' => 'type',
+                        'type' => 'list(foo_types)',
+                        'required' => '1',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'gender' => [
+                        'name' => 'gender',
+                        'type' => 'list(genders)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'city' => [
+                        'name' => 'city',
+                        'type' => 'list(cities)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'country' => [
+                        'name' => 'country',
+                        'type' => 'list(countries)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'cost' => [
+                        'name' => 'cost',
+                        'type' => 'money(currencies)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'birthdate' => [
+                        'name' => 'birthdate',
+                        'type' => 'date',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'created' => [
+                        'name' => 'created',
+                        'type' => 'datetime',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'modified' => [
+                        'name' => 'modified',
+                        'type' => 'datetime',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ]
+                ]
+            ]
+        ];
+    }
+}

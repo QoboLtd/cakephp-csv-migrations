c22768948602a39831e0c7e781f7974cd9705484
George Constantinou <g.constantinou@qobo.biz>
2016-06-24 16:22:52 +0300

    add unit tests for CsvField class (task #2216)

---
+++ tests/TestCase/FieldHandlers/CsvFieldTest.php
@@ -0,0 +1,271 @@
+<?php
+namespace CsvMigrations\Test\TestCase\FieldHandlers;
+
+use Cake\Core\Configure;
+use CsvMigrations\FieldHandlers\CsvField;
+use CsvMigrations\MigrationTrait;
+use PHPUnit_Framework_TestCase;
+
+class CsvFieldTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $dir = dirname(__DIR__) . DS . '..' . DS . 'data' . DS . 'CsvMigrations' . DS . 'migrations' . DS;
+        Configure::write('CsvMigrations.migrations.path', $dir);
+        Configure::write('CsvMigrations.migrations.filename', 'migration.dist');
+
+        $mockTrait = $this->getMockForTrait(MigrationTrait::class);
+        $this->csvData = $mockTrait->getFieldsDefinitions('Foo');
+    }
+
+    /**
+     * @dataProvider nameSetterProvider
+     */
+    public function testSetName($expected, $name)
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setName($name);
+        $this->assertEquals($expected, $csvField->getName());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetNameThrowsException()
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setName('');
+    }
+
+    public function testGetName()
+    {
+        foreach ($this->getterProvider() as $v) {
+            $csvField = new CsvField(array_shift($this->csvData));
+            $this->assertEquals($v[0], $csvField->getName());
+        }
+    }
+
+    /**
+     * @dataProvider typeSetterProvider
+     */
+    public function testSetType($expected, $type)
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setType($type);
+        $this->assertEquals($expected, $csvField->getType());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetTypeEmptyValueThrowsException()
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setType('');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetTypeUnsupportedValueThrowsException()
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setType('foobar');
+    }
+
+    public function testGetType()
+    {
+
+        foreach ($this->getterProvider() as $v) {
+            $csvField = new CsvField(array_shift($this->csvData));
+            $this->assertEquals($v[1], $csvField->getType());
+        }
+    }
+
+    /**
+     * @dataProvider limitSetterProvider
+     */
+    public function testSetLimit($expected, $type)
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setLimit($type);
+        $this->assertEquals($expected, $csvField->getLimit());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetLimitEmptyValueThrowsException()
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setLimit('');
+    }
+
+    public function testGetLimit()
+    {
+
+        foreach ($this->getterProvider() as $v) {
+            $csvField = new CsvField(array_shift($this->csvData));
+            $this->assertEquals($v[2], $csvField->getLimit());
+        }
+    }
+
+    public function testGetAssocCsvModule()
+    {
+
+        foreach ($this->getterProvider() as $v) {
+            $csvField = new CsvField(array_shift($this->csvData));
+            $this->assertEquals($v[2], $csvField->getAssocCsvModule());
+        }
+    }
+
+    /**
+     * @dataProvider booleanSetterProvider
+     */
+    public function testSetRequired($expected, $boolean)
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setRequired($boolean);
+        $this->assertEquals($expected, $csvField->getRequired());
+    }
+
+    public function testGetRequired()
+    {
+
+        foreach ($this->getterProvider() as $v) {
+            $csvField = new CsvField(array_shift($this->csvData));
+            $this->assertEquals($v[3], $csvField->getRequired());
+        }
+    }
+
+    /**
+     * @dataProvider booleanSetterProvider
+     */
+    public function testSetNonSearchable($expected, $boolean)
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setNonSearchable($boolean);
+        $this->assertEquals($expected, $csvField->getNonSearchable());
+    }
+
+    public function testGetNonSearchable()
+    {
+
+        foreach ($this->getterProvider() as $v) {
+            $csvField = new CsvField(array_shift($this->csvData));
+            $this->assertEquals($v[4], $csvField->getNonSearchable());
+        }
+    }
+
+    /**
+     * @dataProvider booleanSetterProvider
+     */
+    public function testSetUnique($expected, $boolean)
+    {
+        $csvField = new CsvField(current($this->csvData));
+        $csvField->setUnique($boolean);
+        $this->assertEquals($expected, $csvField->getUnique());
+    }
+
+    public function testGetUnique()
+    {
+
+        foreach ($this->getterProvider() as $v) {
+            $csvField = new CsvField(array_shift($this->csvData));
+            $this->assertEquals($v[5], $csvField->getUnique());
+        }
+    }
+
+    public function getterProvider()
+    {
+        return [
+            ['id', 'uuid', '', '', '', false],
+            ['description', 'text', '', '', '', false],
+            ['name', 'string', '', '1', '', false],
+            ['status', 'list', 'foo_statuses', '1', '', false],
+            ['type', 'list', 'foo_types', '1', '', false],
+            ['gender', 'list', 'genders', '', '', false],
+            ['city', 'list', 'cities', '', '', false],
+            ['country', 'list', 'countries', '', '', false],
+            ['cost', 'money', 'currencies', '', '', false],
+            ['birthdate', 'date', '', '', '', false],
+            ['created', 'datetime', '', '', '', false],
+            ['modified', 'datetime', '', '', '', false]
+        ];
+    }
+
+    public function nameSetterProvider()
+    {
+        return [
+            ['foo', 'foo'],
+            ['bar', 'bar'],
+            ['FooBar', 'FooBar'],
+            [123, 123],
+            ['123', '123'],
+            [['foo'], ['foo']]
+        ];
+    }
+
+    public function typeSetterProvider()
+    {
+        return [
+            ['related', 'related(Foobar)'],
+            ['has_many', 'has_many(Foobar)'],
+            ['money', 'money(Foobar)'],
+            ['metric', 'metric(Foobar)'],
+            ['list', 'list(Foobar)'],
+            ['datetime', 'datetime'],
+            ['date', 'date'],
+            ['time', 'time'],
+            ['text', 'text'],
+            ['uuid', 'uuid'],
+            ['boolean', 'boolean'],
+            ['string', 'string'],
+            ['integer', 'integer'],
+            ['file', 'file']
+        ];
+    }
+
+    public function limitSetterProvider()
+    {
+        return [
+            ['Foobar', 'related(Foobar)'],
+            ['Foobar', 'has_many(Foobar)'],
+            ['Foobar', 'money(Foobar)'],
+            ['Foobar', 'metric(Foobar)'],
+            ['Foobar', 'list(Foobar)'],
+            [null, 'datetime'],
+            [null, 'date'],
+            [null, 'time'],
+            [null, 'text'],
+            [null, 'uuid'],
+            [35, 'uuid(35)'],
+            [null, 'boolean'],
+            [10, 'boolean(10)'],
+            [255, 'string(255)'],
+            [100, 'string(100)'],
+            [null, 'string'],
+            [null, 'integer'],
+            [5, 'integer(5)'],
+            [11, 'integer(11)'],
+            [null, 'file']
+        ];
+    }
+
+    public function booleanSetterProvider()
+    {
+        return [
+            [true, 1],
+            [true, 'foo'],
+            [true, 100],
+            [true, true],
+            [true, ['foo']],
+            [false, 0],
+            [false, false],
+            [false, null],
+            [false, []]
+        ];
+    }
+}

3667dcb2047195bfed30d8674ebf626c6663619b
George Constantinou <g.constantinou@qobo.biz>
2016-07-01 10:52:08 +0300

    test fields definitions

--- tests/TestCase/Model/Table/FooTableTest.php
+++ tests/TestCase/Model/Table/FooTableTest.php
@@ -120,6 +120,14 @@ class FooTableTest extends TestCase
         $this->assertSame($this->FooTable->getSearchableFieldProperties([]), []);
     }
 
+    /**
+     * @dataProvider fieldsDefinitionsProvider
+     */
+    public function testGetFieldsDefinitions($name, $expected)
+    {
+        $this->assertEquals($expected, $this->FooTable->getFieldsDefinitions($name));
+    }
+
     public function moduleAliasProvider()
     {
         return [
@@ -204,6 +212,101 @@ class FooTableTest extends TestCase
                     ],
                     'modified' => [
                         'type' => 'datetime'
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    public function fieldsDefinitionsProvider()
+    {
+        return [
+            [
+                null,
+                [
+                    'id' => [
+                        'name' => 'id',
+                        'type' => 'uuid',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'description' => [
+                        'name' => 'description',
+                        'type' => 'text',
+                        'required' => '',
+                        'non-searchable' => true,
+                        'unique' => false
+                    ],
+                    'name' => [
+                        'name' => 'name',
+                        'type' => 'string',
+                        'required' => '1',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'status' => [
+                        'name' => 'status',
+                        'type' => 'list(foo_statuses)',
+                        'required' => '1',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'type' => [
+                        'name' => 'type',
+                        'type' => 'list(foo_types)',
+                        'required' => '1',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'gender' => [
+                        'name' => 'gender',
+                        'type' => 'list(genders)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'city' => [
+                        'name' => 'city',
+                        'type' => 'list(cities)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'country' => [
+                        'name' => 'country',
+                        'type' => 'list(countries)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'cost' => [
+                        'name' => 'cost',
+                        'type' => 'money(currencies)',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'birthdate' => [
+                        'name' => 'birthdate',
+                        'type' => 'date',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'created' => [
+                        'name' => 'created',
+                        'type' => 'datetime',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
+                    ],
+                    'modified' => [
+                        'name' => 'modified',
+                        'type' => 'datetime',
+                        'required' => '',
+                        'non-searchable' => '',
+                        'unique' => false
                     ]
                 ]
             ]

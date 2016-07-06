fce2e559d64b1d4c5d207bb6f02d85d1415b5b7b
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-07-06 14:44:43 +0300

    Added more unit tests for field handlers

---
+++ tests/TestCase/FieldHandlers/DefaultFieldHandlerTest.php
@@ -0,0 +1,46 @@
+<?php
+namespace CsvMigrations\Test\TestCase\FieldHandlers;
+
+use CsvMigrations\FieldHandlers\DefaultFieldHandler;
+use PHPUnit_Framework_TestCase;
+
+class DefaultFieldHandlerTest extends PHPUnit_Framework_TestCase
+{
+    protected $fh;
+
+    protected function setUp()
+    {
+        $this->fh = new DefaultFieldHandler();
+    }
+
+    public function testInterface()
+    {
+        $implementedInterfaces = array_keys(class_implements($this->fh));
+        $this->assertTrue(in_array('CsvMigrations\FieldHandlers\FieldHandlerInterface', $implementedInterfaces), "FieldHandlerInterface is not implemented");
+    }
+
+    public function getValues()
+    {
+        return [
+            [true, 'Boolean true'],
+            [false, 'Boolean false'],
+            [0, 'Integer zero'],
+            [1, 'Positive integer'],
+            [-1, 'Negative integer'],
+            [1.501, 'Positive float'],
+            [-1.501, 'Negative float'],
+            ['', 'Empty string'],
+            ['foobar', 'String'],
+            ['2017-07-05', 'Date'],
+        ];
+    }
+
+    /**
+     * @dataProvider getValues
+     */
+    public function testRenderValue($value, $description)
+    {
+        $result = $this->fh->renderValue(null, null, $value, []);
+        $this->assertEquals($value, $result, "Value rendering is broken for: $description");
+    }
+}

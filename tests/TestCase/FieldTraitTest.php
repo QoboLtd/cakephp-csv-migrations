5b25908596171e0ef95984d229d7a60fb614299a
George Constantinou <g.constantinou@qobo.biz>
2016-07-15 16:23:54 +0300

    unit test FieldTrait

---
+++ tests/TestCase/FieldTraitTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace CsvMigrations\Test\TestCase;
+
+use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use CsvMigrations\FieldTrait;
+
+class FieldTraitTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var CsvMigrations\Test\TestCase\Model\Table\FooTable
+     */
+    public $FooTable;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $dir = dirname(__DIR__) . DS . '..' . DS . '..' . DS . 'data' . DS . 'CsvMigrations' . DS;
+
+        /*
+        point to test data
+         */
+        Configure::write('CsvMigrations.migrations.path', $dir . 'migrations' . DS);
+        Configure::write('CsvMigrations.lists.path', $dir . 'lists' . DS);
+        Configure::write('CsvMigrations.migrations.filename', 'migration.dist');
+        $config = TableRegistry::exists('Foo') ? [] : ['className' => 'CsvMigrations\Test\TestCase\Model\Table\FooTable'];
+        $this->FooTable = TableRegistry::get('Foo', $config);
+        $this->mock = $this->getMockForTrait(FieldTrait::class);
+    }
+
+    public function testGetUniqueFields()
+    {
+        $this->assertEquals(['name', 'id'], $this->mock->getUniqueFields($this->FooTable));
+    }
+}

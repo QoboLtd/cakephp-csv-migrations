5729b404247b277ac989beefceaaea3ca9913ae9
Antonis Flangofas <a.flangofas@qobo.biz>
2016-08-30 12:02:37 +0300

    Migrations (task #2417)

---
+++ config/Migrations/20160830084645_CreateDbListItems.php
@@ -0,0 +1,44 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateDbListItems extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('db_list_items', ['id' => false, 'primary_key' => ['id']]);
+        $table->addColumn('id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('db_list_id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'null' => false,
+            'limit' => 255,
+        ]);
+        $table->addColumn('value', 'string', [
+            'default' => null,
+            'null' => false,
+            'limit' => 255,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}

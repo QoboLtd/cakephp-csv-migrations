17fc780c631f21a53142e3af2a9a9a6ebd975601
Antonis Flangofas <a.flangofas@qobo.biz>
2016-08-31 02:17:02 +0300

    Created DBListField handler (task #2417)

---
+++ src/FieldHandlers/DbListFieldHandler.php
@@ -0,0 +1,87 @@
+<?php
+namespace CsvMigrations\FieldHandlers;
+
+use CsvMigrations\FieldHandlers\BaseFieldHandler;
+
+class DbListFieldHandler extends BaseFieldHandler
+{
+    /**
+     * Field type
+     */
+    const FIELD_TYPE = 'string';
+
+    /**
+     * Field type match pattern
+     */
+    const FIELD_TYPE_PATTERN = '/list\((.*?)\)/';
+
+    /**
+     * Input field html markup
+     */
+    const INPUT_HTML = '<div class="form-group">%s</div>';
+
+    /**
+     * Method responsible for rendering field's input.
+     *
+     * @param  mixed  $table   name or instance of the Table
+     * @param  string $field   field name
+     * @param  string $data    field data
+     * @param  array  $options field options
+     * @return string          field input
+     */
+    public function renderInput($table, $field, $data = '', array $options = [])
+    {
+        $result = '';
+        //CsvField object is mandatory
+        if (!isset($options['fieldDefinitions']) ||
+            !($options['fieldDefinitions'] instanceof CsvField)) {
+            return $result;
+        }
+        $csvObj = $options['fieldDefinitions'];
+        $list = $csvObj->getListName();
+        $field = $this->_getFieldName($table, $field, $options);
+        $options = [
+            'class' => 'form-control',
+            'value' => $data
+        ];
+        $result = $this->cakeView->cell('CsvMigrations.DbList', [$field, $list, $options])->render();
+
+        return $result;
+    }
+
+    /**
+     * Method that renders list field's value.
+     *
+     * @param  mixed  $table   name or instance of the Table
+     * @param  string $field   field name
+     * @param  string $data    field data
+     * @param  array  $options field options
+     * @return string
+     */
+    public function renderValue($table, $field, $data, array $options = [])
+    {
+        $result = $data;
+
+        return $result;
+    }
+
+    /**
+     * Method responsible for converting csv field instance to database field instance.
+     *
+     * @param  \CsvMigrations\FieldHandlers\CsvField $csvField CsvField instance
+     * @return array list of DbField instances
+     */
+    public function fieldToDb(CsvField $csvField)
+    {
+        $dbFields[] = new DbField(
+            $csvField->getName(),
+            static::FIELD_TYPE,
+            null,
+            $csvField->getRequired(),
+            $csvField->getNonSearchable(),
+            $csvField->getUnique()
+        );
+
+        return $dbFields;
+    }
+}

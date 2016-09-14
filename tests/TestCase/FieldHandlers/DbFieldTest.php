12317a9d8f29e4fd67817472afbed8394e4d98bb
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-09-14 13:51:05 +0300

    Added unit tests for DbField class

---
+++ tests/TestCase/FieldHandlers/DbFieldTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace CsvMigrations\Test\TestCase\FieldHandlers;
+
+use CsvMigrations\FieldHandlers\DbField;
+use PHPUnit_Framework_TestCase;
+
+class DbFieldTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $dbField = new DbField('field', 'string', 255, true, true, true);
+
+        $this->assertEquals('field', $dbField->getName(), "Field name was not properly set");
+        $this->assertEquals('string', $dbField->getType(), "Field type was not properly set");
+        $this->assertEquals(255, $dbField->getLimit(), "Field limit was not properly set");
+        $this->assertTrue($dbField->getRequired(), "Field required was not properly set");
+        $this->assertTrue($dbField->getNonSearchable(), "Field non-searchable was not properly set");
+        $this->assertTrue($dbField->getUnique(), "Field unique was not properly set");
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetNameEmptyException()
+    {
+        $dbField = new DbField('', 'string', 255, true, true, true);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetTypeEmptyException()
+    {
+        $dbField = new DbField('field', '', 255, true, true, true);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testSetTypeUnsupportedException()
+    {
+        $dbField = new DbField('field', 'unsupported-field-type', 255, true, true, true);
+    }
+}

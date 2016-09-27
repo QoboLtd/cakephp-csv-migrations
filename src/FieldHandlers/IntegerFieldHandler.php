1dc09acd923e6c6832117a75a2413eec14fa4ae3
George Constantinou <g.constantinou@qobo.biz>
2016-09-27 13:57:01 +0300

    added number format on Integer Field Handler (task #2627)

--- src/FieldHandlers/IntegerFieldHandler.php
+++ src/FieldHandlers/IntegerFieldHandler.php
@@ -5,4 +5,17 @@ use CsvMigrations\FieldHandlers\BaseFieldHandler;
 
 class IntegerFieldHandler extends BaseFieldHandler
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function renderValue($table, $field, $data, array $options = [])
+    {
+        $result = $data;
+
+        if (!empty($data) && is_numeric($data)) {
+            $result = number_format($data);
+        }
+
+        return $result;
+    }
 }

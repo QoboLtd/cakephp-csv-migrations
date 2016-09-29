568283a0ab39c31a359f853ffd0ba4e99dc0bc88
George Constantinou <g.constantinou@qobo.biz>
2016-09-29 18:48:54 +0300

    added plugin configuration parameters for API, menus and events (task #2629)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -11,6 +11,31 @@ Configure::write('CsvMigrations.lists.path', CONFIG . 'CsvMigrations' . DS . 'li
 Configure::write('CsvMigrations.migrations.filename', 'migration');
 Configure::write('CsvMigrations.typeahead.min_length', 1);
 Configure::write('CsvMigrations.typeahead.timeout', 300);
+Configure::write('CsvMigrations.api', [
+    'menus_property' => '_Menus',
+    'excluded_menus' => [
+        'index' => ['top']
+    ]
+]);
+Configure::write('CsvMigrations.menus', [
+    'index' => ['top', 'actions'],
+    'view' => ['top', 'top-row', 'associated']
+]);
+Configure::write('CsvMigrations.events', [
+    'index' => [
+        'menus' => [
+            'top' => ['View.Index.Menu.Top' => 'getIndexMenuTop'],
+            'actions' => ['View.Index.Menu.Actions' => 'getIndexMenuActions']
+        ]
+    ],
+    'view' => [
+        'menus' => [
+            'top' => ['View.View.Menu.Top' => 'getViewMenuTop'],
+            'top-row' => ['View.View.Menu.Top.Row' => 'getViewMenuTopRow'],
+            'associated' => ['View.Associated.Menu.Actions' => 'getAssociatedMenuActions']
+        ]
+    ]
+]);
 
 EventManager::instance()->on(new ViewMenuListener());
 

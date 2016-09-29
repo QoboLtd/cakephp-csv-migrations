fa053672de77c607c37d00e1da8d0e5030c67680
George Constantinou <g.constantinou@qobo.biz>
2016-09-29 11:47:45 +0300

    extend Application's AppController to inherit all the logic, like plugin's AppController does (task #2629)

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -1,7 +1,7 @@
 <?php
 namespace CsvMigrations\Controller\Api;
 
-use Cake\Controller\Controller;
+use App\Controller\AppController as BaseController;
 use Cake\Core\Configure;
 use Cake\Datasource\ResultSetDecorator;
 use Cake\Event\Event;
@@ -16,7 +16,7 @@ use CsvMigrations\Panel;
 use CsvMigrations\PanelUtilTrait;
 use CsvMigrations\PrettifyTrait;
 
-class AppController extends Controller
+class AppController extends BaseController
 {
     /**
      * Pretty format identifier

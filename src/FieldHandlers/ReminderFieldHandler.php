faa2c1773811fc9cf5ae943f582df55bddf926c5
anvyst <jujav4ik@gmail.com>
2016-10-13 11:59:44 +0300

    Reminder extends Datetime avoiding repetitive code (task #2749)

--- src/FieldHandlers/ReminderFieldHandler.php
+++ src/FieldHandlers/ReminderFieldHandler.php
@@ -2,83 +2,8 @@
 namespace CsvMigrations\FieldHandlers;
 
 use Cake\I18n\Date;
-use CsvMigrations\FieldHandlers\BaseFieldHandler;
+use CsvMigrations\FieldHandlers\DatetimeFieldHandler;
 
-class ReminderFieldHandler extends BaseFieldHandler
+class ReminderFieldHandler extends DatetimeFieldHandler
 {
-    /**
-     * Database field type
-     */
-    const DB_FIELD_TYPE = 'datetime';
-
-    /**
-     * Input field type
-     */
-    const INPUT_FIELD_TYPE = 'datetimepicker';
-
-    /**
-     * Reminder format
-     */
-    const DATETIME_FORMAT = 'yyyy-MM-dd HH:mm';
-
-    /**
-     * Method responsible for rendering field's input.
-     *
-     * @param  mixed  $table   name or instance of the Table
-     * @param  string $field   field name
-     * @param  string $data    field data
-     * @param  array  $options field options
-     * @return string          field input
-     */
-    public function renderInput($table, $field, $data = '', array $options = [])
-    {
-        if ($data instanceof Date) {
-            $data = $data->i18nFormat(static::DATETIME_FORMAT);
-        }
-
-        $required = (bool)$options['fieldDefinitions']->getRequired();
-        $fieldName = $this->_getFieldName($table, $field, $options);
-
-        if (!isset($options['element']) && $this->cakeView->elementExists('QoboAdminPanel.datepicker')) {
-            $options['element'] = 'QoboAdminPanel.datepicker';
-        }
-
-        if (isset($options['element'])) {
-            return $this->cakeView->element($options['element'], [
-                'options' => [
-                    'fieldName' => $fieldName,
-                    'type' => static::INPUT_FIELD_TYPE,
-                    'label' => true,
-                    'required' => $required,
-                    'value' => $data
-                ]
-            ]);
-        } else {
-            return $this->cakeView->Form->input($fieldName, [
-                'type' => 'datetime',
-                'required' => $required,
-                'value' => $data
-            ]);
-        }
-    }
-
-    /**
-     * Method that renders default type field's value.
-     *
-     * @param  mixed  $table   name or instance of the Table
-     * @param  string $field   field name
-     * @param  string $data    field data
-     * @param  array  $options field options
-     * @return string
-     */
-    public function renderValue($table, $field, $data, array $options = [])
-    {
-        if (is_object($data)) {
-            $result = $data->i18nFormat(static::DATETIME_FORMAT);
-        } else {
-            $result = $data;
-        }
-
-        return $result;
-    }
 }

a13bafde7fe0d7fd4ef4b2a6f8e5ce483effe794
George Constantinou <g.constantinou@qobo.biz>
2016-10-20 18:59:10 +0300

    handle precision and scale for decimal fields from csv migrations limit (task #2839)

--- src/FieldHandlers/DecimalFieldHandler.php
+++ src/FieldHandlers/DecimalFieldHandler.php
@@ -23,4 +23,39 @@ class DecimalFieldHandler extends BaseFieldHandler
 
         return $result;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function fieldToDb(CsvField $csvField)
+    {
+        $dbFields[] = new DbField(
+            $csvField->getName(),
+            static::DB_FIELD_TYPE,
+            $csvField->getLimit(),
+            $csvField->getRequired(),
+            $csvField->getNonSearchable(),
+            $csvField->getUnique()
+        );
+
+        // set precision and scale provided by csv migration decimal field type definition
+        foreach($dbFields as &$dbField) {
+            // skip if scale and precision are not defined
+            if (empty($dbField->getLimit())) {
+                continue;
+            }
+            // skip if scale and precision are not defined correctly
+            if (false === strpos($dbField->getLimit(), '.')) {
+                continue;
+            }
+
+            list($precision, $scale) = explode('.', $dbField->getLimit());
+            $options = $dbField->getOptions();
+            $options['precision'] = $precision;
+            $options['scale'] = $scale;
+            $dbField->setOptions($options);
+        }
+
+        return $dbFields;
+    }
 }

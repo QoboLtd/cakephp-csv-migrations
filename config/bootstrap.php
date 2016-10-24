2193565b4f206293ba442fe82a6500f1b9df73b6
Andrey Vystavkin <jujav4ik@gmail.com>
2016-10-24 14:29:13 +0300

    adding missing reports ini filename spec for (task #2686)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -15,6 +15,7 @@ Configure::write('CsvMigrations.migrations.path', CONFIG . 'CsvMigrations' . DS 
 Configure::write('CsvMigrations.views.path', CONFIG . 'CsvMigrations' . DS . 'views' . DS);
 Configure::write('CsvMigrations.lists.path', CONFIG . 'CsvMigrations' . DS . 'lists' . DS);
 Configure::write('CsvMigrations.migrations.filename', 'migration');
+Configure::write('CsvMigrations.reports.filename', 'reports');
 Configure::write('CsvMigrations.typeahead.min_length', 1);
 Configure::write('CsvMigrations.typeahead.timeout', 300);
 Configure::write('CsvMigrations.acl', [

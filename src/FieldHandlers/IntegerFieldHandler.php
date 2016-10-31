e85dae5baad0421cac597cc60a668821aa1c8167
George Constantinou <g.constantinou@qobo.biz>
2016-10-31 11:39:00 +0200

    type cast value to integer (task #2857)

--- src/FieldHandlers/IntegerFieldHandler.php
+++ src/FieldHandlers/IntegerFieldHandler.php
@@ -15,7 +15,7 @@ class IntegerFieldHandler extends BaseFieldHandler
      */
     public function renderValue($table, $field, $data, array $options = [])
     {
-        $result = filter_var($data, FILTER_SANITIZE_NUMBER_INT);
+        $result = (int)filter_var($data, FILTER_SANITIZE_NUMBER_INT);
 
         if (!empty($result) && is_numeric($result)) {
             $result = number_format($result);

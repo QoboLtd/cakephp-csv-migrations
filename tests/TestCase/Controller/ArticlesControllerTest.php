af4bd6b6812007275e161ebe5b7610797254b11b
George Constantinou <g.constantinou@qobo.biz>
2016-11-09 11:22:35 +0200

    integration tests for Add and Edit actions

--- tests/TestCase/Controller/ArticlesControllerTest.php
+++ tests/TestCase/Controller/ArticlesControllerTest.php
@@ -34,4 +34,43 @@ class ArticlesControllerTest extends IntegrationTestCase
         $this->assertResponseContains('Modified:');
         $this->assertResponseContains('Foo');
     }
+
+    public function testAdd()
+    {
+        $this->session([
+            'Auth.User.id' => '00000000-0000-0000-0000-000000000001'
+        ]);
+
+        $this->get('/articles/add');
+        $this->assertResponseOk();
+        // form element and attributes
+        $this->assertResponseContains('<form');
+        $this->assertResponseContains('action="/articles/add"');
+        $this->assertResponseContains('data-panels-url="/api/articles/panels"');
+        // submit button
+        $this->assertResponseContains('type="submit"');
+        // input element(s) and attributes
+        $this->assertResponseContains('Name');
+        $this->assertResponseContains('name="Articles[name]"');
+    }
+
+    public function testEdit()
+    {
+        $this->session([
+            'Auth.User.id' => '00000000-0000-0000-0000-000000000001'
+        ]);
+
+        $this->get('/articles/edit/d8c3ba90-c418-4e58-8cb6-b65c9095a2dc');
+        $this->assertResponseOk();
+        // form element and attributes
+        $this->assertResponseContains('<form');
+        $this->assertResponseContains('action="/articles/edit/d8c3ba90-c418-4e58-8cb6-b65c9095a2dc"');
+        $this->assertResponseContains('data-panels-url="/api/articles/panels"');
+        // submit button
+        $this->assertResponseContains('type="submit"');
+        // input element(s) and attributes
+        $this->assertResponseContains('Name');
+        $this->assertResponseContains('name="Articles[name]"');
+        $this->assertResponseContains('value="Foo"');
+    }
 }

a28fa6f96b06c7d9f536bddc9d92635aa77e0d02
George Constantinou <g.constantinou@qobo.biz>
2016-11-11 11:56:56 +0200

    moved find by lookup-field logic into a Trait (task #3014)

--- src/FieldTrait.php
+++ src/FieldTrait.php
@@ -1,6 +1,35 @@
 <?php
 namespace CsvMigrations;
 
+use Cake\ORM\Query;
+
 trait FieldTrait
 {
+    /**
+     * Method that adds lookup fields with the id value to the Where clause in ORM Query
+     *
+     * @param  \Cake\ORM\Query $query Query instance
+     * @param  string          $id    Record id
+     * @return \Cake\ORM\Query
+     */
+    public function findByLookupFields(Query $query, $id)
+    {
+        $lookupFields = $this->lookupFields();
+
+        if (empty($lookupFields)) {
+            return $query;
+        }
+
+        $tableName = $this->alias();
+        // check for record by table's lookup fields
+        foreach ($lookupFields as $lookupField) {
+            // prepend table name to avoid CakePHP ORM's ambiguous column errors
+            if (false === strpos($lookupField, '.')) {
+                $lookupField = $tableName . '.' . $lookupField;
+            }
+            $query->orWhere([$lookupField => $id]);
+        }
+
+        return $query;
+    }
 }

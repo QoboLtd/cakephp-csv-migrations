ac52048fbcb94d4398e5addcccaf6535e6dd6ff3
George Constantinou <g.constantinou@qobo.biz>
2016-12-22 17:28:39 +0200

    cs fixes

--- webroot/js/jquery.dynamicSelectInit.js
+++ webroot/js/jquery.dynamicSelectInit.js
@@ -13,34 +13,31 @@
 
         init: function () {
 
-        // loop through dynamic-select inputs
-        $(this.options.id).each(function () {
-            var structure = $(this).data('structure');
-            var option_values = $(this).data('option-values');
-            var selectors = $(this).data('selectors');
-            var hide_next = $(this).data('hide-next');
-            var previous_default_value = $(this).data('previous-default-value');
-            console.log(structure);
-            console.log(option_values);
-            console.log(selectors);
-            $(document).dynamicSelect({
-                structure: structure,
-                optionValues: option_values,
-                selectors: selectors,
-                hideNext: hide_next,
-                previousDefaultValue: previous_default_value
-            });
+            // loop through dynamic-select inputs
+            $(this.options.id).each(function () {
+                var structure = $(this).data('structure');
+                var option_values = $(this).data('option-values');
+                var selectors = $(this).data('selectors');
+                var hide_next = $(this).data('hide-next');
+                var previous_default_value = $(this).data('previous-default-value');
+                $(document).dynamicSelect({
+                    structure: structure,
+                    optionValues: option_values,
+                    selectors: selectors,
+                    hideNext: hide_next,
+                    previousDefaultValue: previous_default_value
+                });
 
-            // set values on edit mode
-            selectors.forEach(function (el) {
-                var value = $(el).data('value');
-                if (!value) {
-                    return;
-                }
+                // set values on edit mode
+                selectors.forEach(function (el) {
+                    var value = $(el).data('value');
+                    if (!value) {
+                        return;
+                    }
 
-                $(el).val(value).change();
+                    $(el).val(value).change();
+                });
             });
-        });
         }
     };
 

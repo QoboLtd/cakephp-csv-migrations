cbbb7210b43ac5ac949c227458fd75865dcdab81
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-01-18 18:53:55 +0200

    Updated unit tests (task #3206)
    Updated unit tests and test data to support the new CSV and
    INI locations.

--- tests/csv_migrations.php
+++ tests/csv_migrations.php
@@ -2,10 +2,7 @@
 use Cake\Core\Configure;
 
 Configure::write('CsvMigrations.actions', ['index', 'view', 'add', 'edit']);
-Configure::write('CsvMigrations.migrations.path', CONFIG . 'CsvMigrations' . DS . 'migrations' . DS);
-Configure::write('CsvMigrations.views.path', CONFIG . 'CsvMigrations' . DS . 'views' . DS);
-Configure::write('CsvMigrations.lists.path', CONFIG . 'CsvMigrations' . DS . 'lists' . DS);
-Configure::write('CsvMigrations.migrations.filename', 'migration');
+Configure::write('CsvMigrations.modules.path', CONFIG . 'Modules' . DS);
 Configure::write('CsvMigrations.api', [
     'auth' => true,
     'token' => null

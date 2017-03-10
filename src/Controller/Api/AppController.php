ce670d4d476d72bfb3b5232d1594ef56800e41fe
George Constantinou <g.constantinou@qobo.biz>
2017-03-10 12:53:45 +0200

    static method that is called to generate swagger annotations on the fly (task #2246)

--- src/Controller/Api/AppController.php
+++ src/Controller/Api/AppController.php
@@ -11,6 +11,7 @@ use CsvMigrations\CsvMigrationsUtils;
 use CsvMigrations\FileUploadsUtils;
 use CsvMigrations\Panel;
 use CsvMigrations\PanelUtilTrait;
+use CsvMigrations\Swagger\Annotation;
 use ReflectionMethod;
 
 class AppController extends Controller
@@ -424,5 +425,21 @@ class AppController extends Controller
         }
 
         return $result;
+    }
+
+    /**
+     * Generates Swagger annotations
+     *
+     * Instantiates CsvAnnotation with required parameters
+     * and returns its generated swagger annotation content.
+     *
+     * @param string $path File path
+     * @return string
+     */
+    public static function generateSwaggerAnnotations($path)
+    {
+        $csvAnnotation = new Annotation(get_called_class(), $path);
+
+        return $csvAnnotation->getContent();
     }
 }

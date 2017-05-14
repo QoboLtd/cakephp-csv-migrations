e44aee203c5c073bfbde47d842925a7d0185119d
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-05-14 16:30:24 +0300

    BaseStringFieldHandler uses BaseRenderer for sanitization (task #3809)

--- src/FieldHandlers/BaseStringFieldHandler.php
+++ src/FieldHandlers/BaseStringFieldHandler.php
@@ -5,13 +5,4 @@ use CsvMigrations\FieldHandlers\BaseSimpleFieldHandler;
 
 abstract class BaseStringFieldHandler extends BaseSimpleFieldHandler
 {
-    /**
-     * Sanitize options
-     *
-     * Name of filter_var() filter to run and all desired
-     * options/flags.
-     *
-     * @var array
-     */
-    public $sanitizeOptions = [FILTER_SANITIZE_STRING];
 }

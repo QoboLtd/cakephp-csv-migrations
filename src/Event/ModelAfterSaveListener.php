bc0c3d9df4e83eb610b343a6feb1faab23adb4ca
Leonid Mamchenkov <leonid@mamchenkov.net>
2017-08-11 15:46:39 +0300

    Refactored some code into DTZone and IcEmail

--- src/Event/ModelAfterSaveListener.php
+++ src/Event/ModelAfterSaveListener.php
@@ -5,34 +5,18 @@ use Cake\Datasource\EntityInterface;
 use Cake\Event\Event;
 use Cake\Event\EventListenerInterface;
 use Cake\I18n\Time;
-use Cake\Mailer\Email;
 use Cake\ORM\Table;
-use Cake\Routing\Router;
 use Cake\Utility\Inflector;
 use CsvMigrations\ConfigurationTrait;
-use CsvMigrations\FieldHandlers\FieldHandlerFactory;
 use CsvMigrations\Table as CsvTable;
-use CsvMigrations\Utility\ICal\IcCalendar;
-use CsvMigrations\Utility\ICal\IcEvent;
-use DateTime;
+use CsvMigrations\Utility\DTZone;
+use CsvMigrations\Utility\ICal\IcEmail;
 use DateTimeZone;
 use Exception;
 use InvalidArgumentException;
 
 class ModelAfterSaveListener implements EventListenerInterface
 {
-    /**
-     * Changelog template
-     */
-    const CHANGELOG = '* %s: changed from "%s" to "%s".' . "\n";
-
-    /**
-     * Ingored modified fields
-     *
-     * @var array
-     */
-    protected $ignoredFields = ['created', 'modified'];
-
     /**
      * End date fields
      *
@@ -93,8 +77,9 @@ class ModelAfterSaveListener implements EventListenerInterface
             // get attendee emails
             $emails = $this->getAttendees($table, $entity, $attendeesFields);
             // get email subject and content
-            $emailSubject = $this->getEmailSubject($table, $entity);
-            $emailContent = $this->getEmailContent($table, $entity);
+            $mailer = new IcEmail($table, $entity);
+            $emailSubject = $mailer->getEmailSubject();
+            $emailContent = $mailer->getEmailContent();
             // get common event options
             $eventOptions = $this->getEventOptions($table, $entity, $reminderField);
             // Set same attendees for all
@@ -110,7 +95,7 @@ class ModelAfterSaveListener implements EventListenerInterface
             $eventOptions['organizer'] = $email;
 
             try {
-                $sent = $this->sendCalendarEmail($email, $emailSubject, $emailContent, $eventOptions);
+                $sent = $mailer->sendCalendarEmail($email, $emailSubject, $emailContent, $eventOptions);
             } catch (\Exception $e) {
                 $sent = $e;
             }
@@ -131,6 +116,7 @@ class ModelAfterSaveListener implements EventListenerInterface
      *
      * @throws \InvalidArgumentException If $table is not an instance of CsvTable
      * @param object $table Instance of a table class
+     * @return void
      */
     protected function checkCsvTable($table)
     {
@@ -326,228 +312,6 @@ class ModelAfterSaveListener implements EventListenerInterface
     }
 
     /**
-     * Get email subject
-     *
-     * @param \CsvMigrations\Table $table Table instance
-     * @param \Cake\ORM\EntityInterface $entity Entity instance
-     * @return string
-     */
-    protected function getEmailSubject(CsvTable $table, EntityInterface $entity)
-    {
-        $module = Inflector::singularize($table->alias());
-        $displayValue = $this->getDisplayValue($table, $entity);
-        $result = $module . ": " . $displayValue;
-
-        if (!$entity->isNew()) {
-            $result = "(Updated) " . $result;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Get email content
-     *
-     * @param \CsvMigrations\Table $table Table instance
-     * @param \Cake\ORM\EntityInterface $entity Entity instance
-     * @return string
-     */
-    protected function getEmailContent(CsvTable $table, EntityInterface $entity)
-    {
-        $result = '';
-
-        $module = Inflector::singularize($table->alias());
-        $displayValue = $this->getDisplayValue($table, $entity);
-        $user = $this->getUserString($table);
-        $action = $entity->isNew() ? 'created' : 'updated';
-
-        // Example: Lead Foobar created by System
-        $result = sprintf("%s %s %s by %s", $module, $displayValue, $action, $user);
-
-        $changeLog = $this->getChangelog($entity);
-        if (!empty($changeLog)) {
-            $result .= "\n\n";
-            $result .= $changeLog;
-            $result .= "\n";
-        }
-
-        $result .= "\n\n";
-        $result .= "See more: ";
-        $result .= $this->getEntityUrl($table, $entity);
-
-        return $result;
-    }
-
-    /**
-     * Get event subject/summary
-     *
-     * For now this is just an alias for getEmailSubject().
-     *
-     * @param \CsvMigrations\Table $table Table instance
-     * @param \Cake\ORM\EntityInterface $entity Entity instance
-     * @return string
-     */
-    protected function getEventSubject(CsvTable $table, EntityInterface $entity)
-    {
-        return $this->getEmailSubject($table, $entity);
-    }
-
-    /**
-     * Get event content/description
-     *
-     * @param \CsvMigrations\Table $table Table instance
-     * @param \Cake\ORM\EntityInterface $entity Entity instance
-     * @return string
-     */
-    protected function getEventContent(CsvTable $table, EntityInterface $entity)
-    {
-        $result = '';
-
-        $entityFields = [
-            'description',
-            'agenda',
-            'comments',
-            'comment',
-            'notes',
-        ];
-
-        foreach ($entityFields as $field) {
-            if (!empty($entity->$field)) {
-                $result = $entity->$field;
-                break;
-            }
-        }
-
-        $result .= "\n\n";
-        $result .= "See more: ";
-        $result .= $this->getEntityUrl($table, $entity);
-
-        return $result;
-    }
-
-    /**
-     * Get plain value of the entity display field
-     *
-     * @param \CsvMigrations\Table $table Table instance
-     * @param \Cake\ORM\EntityInterface $entity Entity instance
-     * @return string
-     */
-    protected function getDisplayValue(CsvTable $table, EntityInterface $entity)
-    {
-        try {
-            $displayField = $table->displayField();
-            $displayValue = $entity->{$displayField};
-
-            $fhf = new FieldHandlerFactory();
-            $result = $fhf->renderValue($table, $displayField, $displayValue, [ 'renderAs' => 'plain']);
-            if (empty($result) || !is_string($result)) {
-                throw new InvalidArgumentException("Failed to get entity display value");
-            }
-        } catch (Exception $e) {
-            $result = 'reminder';
-        }
-
-        return $result;
-    }
-
-    /**
-     * Get full URL to the entity view
-     *
-     * @param \CsvMigrations\Table $table Table instance
-     * @param \Cake\ORM\EntityInterface $entity Entity instance
-     * @return string
-     */
-    protected function getEntityUrl(CsvTable $table, EntityInterface $entity)
-    {
-        $result = Router::url(
-            [
-                'prefix' => false,
-                'controller' => $table->table(),
-                'action' => 'view',
-                $entity->id
-            ],
-            true
-        );
-
-        return $result;
-    }
-
-    /**
-     * Get plain value of the current user
-     *
-     * @param \CsvMigrations\Table $table Table instance
-     * @return string
-     */
-    protected function getUserString(CsvTable $table)
-    {
-        $result = 'System';
-
-        $userFields = [
-            'name',
-            'username',
-            'email',
-        ];
-
-        $currentUser = $table->getCurrentUser();
-        if (empty($currentUser) || !is_array($currentUser)) {
-            return $result;
-        }
-
-        foreach ($userFields as $field) {
-            if (!empty($currentUser[$field])) {
-                return $currentUser[$field];
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * Creates changelog report in string format.
-     *
-     * Example:
-     *
-     * Subject: changed from 'Foo' to 'Bar'.
-     * Content: changed from 'Hello world' to 'Hi there'.
-     *
-     * @param \Cake\Datasource\EntityInterface $entity Entity instance
-     * @return string
-     */
-    protected function getChangelog(EntityInterface $entity)
-    {
-        $result = '';
-
-        // plain changelog if entity is new
-        if ($entity->isNew()) {
-            return $result;
-        }
-
-        // get entity's modified fields
-        $fields = $entity->extractOriginalChanged($entity->visibleProperties());
-
-        if (empty($fields)) {
-            return $result;
-        }
-
-        // remove ignored fields
-        foreach ($this->ignoredFields as $field) {
-            if (array_key_exists($field, $fields)) {
-                unset($fields[$field]);
-            }
-        }
-
-        if (empty($fields)) {
-            return $result;
-        }
-
-        foreach ($fields as $k => $v) {
-            $result .= sprintf(static::CHANGELOG, Inflector::humanize($k), $v, $entity->{$k});
-        }
-
-        return $result;
-    }
-
-    /**
      * Get options for the new event
      *
      * @param \CsvMigrations\Table $table Table instance
@@ -560,68 +324,17 @@ class ModelAfterSaveListener implements EventListenerInterface
         // Event start and end times
         $eventTimes = $this->getEventTime($entity, $startField);
 
+        $mailer = new IcEmail($table, $entity);
+
         $result = [
             'id' => $entity->id,
             'sequence' => $entity->isNew() ? 0 : time(),
-            'summary' => $this->getEventSubject($table, $entity),
-            'description' => $this->getEventContent($table, $entity),
+            'summary' => $mailer->getEventSubject(),
+            'description' => $mailer->getEventContent(),
             'location' => $entity->location,
             'startTime' => $eventTimes['start'],
             'endTime' => $eventTimes['end'],
         ];
-
-        return $result;
-    }
-
-    /**
-     * Get iCal calendar with given event
-     *
-     * @param array $eventOptions Options for event creation
-     * @return object Whatever IcCalendar::getCalendar() returns
-     */
-    protected function getEventCalendar(array $eventOptions)
-    {
-        // New iCal event
-        $event = new IcEvent($eventOptions);
-        $event = $event->getEvent();
-
-        // New iCal calendar
-        $calendar = new IcCalendar();
-        $calendar->addEvent($event);
-        $calendar = $calendar->getCalendar();
-
-        return $calendar;
-    }
-
-    /**
-     * Send email with calendar attachment
-     *
-     * @param string $to Destination email address
-     * @param string $subject Email subject
-     * @param string $content Email message content
-     * @param array $eventOptions Event options for calendar attachment
-     * @param string $config Email config to use ('default' if omitted)
-     * @return array Result of Email::send()
-     */
-    protected function sendCalendarEmail($to, $subject, $content, array $eventOptions, $config = 'default')
-    {
-        // Get iCal calendar
-        $calendar = $this->getEventCalendar($eventOptions);
-
-        // FIXME: WTF happened to new lines???
-        $headers = "Content-Type: text/calendar; charset=utf-8";
-        $headers .= 'Content-Disposition: attachment; filename="event.ics"';
-
-        $emailer = new Email($config);
-        $emailer->to($to)
-            ->setHeaders([$headers])
-            ->subject($subject)
-            ->attachments(['event.ics' => [
-                'contentDisposition' => true,
-                'mimetype' => 'text/calendar',
-                'data' => $calendar->render()
-            ]]);
-        $result = $emailer->send($content);
 
         return $result;
     }
@@ -640,10 +353,10 @@ class ModelAfterSaveListener implements EventListenerInterface
     protected function getEventTime(EntityInterface $entity, $startField)
     {
         // Application timezone
-        $dtz = new DateTimeZone($this->getAppTimeZone());
+        $dtz = new DateTimeZone(DTZone::getAppTimeZone());
 
         // We check that the value is always there in sendCalendarReminder()
-        $start = $this->toDateTime($entity->$startField, $dtz);
+        $start = DTZone::toDateTime($entity->$startField, $dtz);
 
         // Default end time is 1 hour from start
         $end = $start;
@@ -653,7 +366,7 @@ class ModelAfterSaveListener implements EventListenerInterface
         if (empty($entity->duration)) {
             foreach ($this->endDateFields as $endField) {
                 if (!empty($entity->{$endField})) {
-                    $end = $this->toDateTime($entity->{$endField}, $dtz);
+                    $end = DTZone::toDateTime($entity->{$endField}, $dtz);
                     break;
                 }
             }
@@ -669,96 +382,13 @@ class ModelAfterSaveListener implements EventListenerInterface
         }
 
         // Adjust to UTC in case custom timezone is used for an app.
-        $start = $this->offsetToUtc($start);
-        $end = $this->offsetToUtc($end);
+        $start = DTZone::offsetToUtc($start);
+        $end = DTZone::offsetToUtc($end);
 
         $result = [
             'start' => $start,
             'end' => $end,
         ];
-
-        return $result;
-    }
-
-    /**
-     * Get application timezone
-     *
-     * If application timezone is not configured,
-     * fallback on UTC.
-     *
-     * @todo Move to Qobo/Utils
-     * @return string Timezone string, like UTC
-     */
-    protected function getAppTimeZone()
-    {
-        $result = 'UTC';
-        $appTimezone = Time::now()->format('e');
-        if (!empty($appTimezone)) {
-            $result = $appTimezone;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Convert a given value to DateTime instance
-     *
-     * @throws \InvalidArgumentException when cannot convert to \DateTime
-     * @param mixed $value Value to convert (string, Time, DateTime, etc)
-     * @param \DateTimeZone $dtz DateTimeZone instance
-     * @return \DateTime
-     */
-    protected function toDateTime($value, DateTimeZone $dtz)
-    {
-        // TODO : Figure out where to move. Can vary for different source objects
-        $format = 'Y-m-d H:i:s';
-
-        if (is_string($value)) {
-            $value = strtotime($value);
-            $value = date($format, $value);
-
-            return new DateTime($value, $dtz);
-        }
-
-        if ($value instanceof Time) {
-            $value = $value->format($format);
-
-            return new DateTime($value, $dtz);
-        }
-
-        if ($value instanceof DateTime) {
-            return $value;
-        }
-
-        throw new InvalidArgumentException("Type [" . gettype($value) . "] is not supported for date/time");
-    }
-
-    /**
-     * Offset DateTime value to UTC
-     *
-     * NOTE: This is a temporary work around until we fix our handling of
-     *       the application timezones.  Database values should always be
-     *       stored in UTC no matter what.  Otherwise, you will be riding
-     *       a bike which is on fire, while you are on fire, and everything
-     *       around you is on fire.  See Redmine ticket #4336 for details.
-     *
-     * @param \DateTime $value DateTime value to offset
-     * @return \DateTime
-     */
-    protected function offsetToUtc(DateTime $value)
-    {
-        $result = $value;
-
-        $dtz = $value->getTimezone();
-        if ($dtz->getName() === 'UTC') {
-            return $result;
-        }
-
-        $epoch = time();
-        $transitions = $dtz->getTransitions($epoch, $epoch);
-
-        $offset = $transitions[0]['offset'];
-        $result = $result->modify("-$offset seconds");
 
         return $result;
     }

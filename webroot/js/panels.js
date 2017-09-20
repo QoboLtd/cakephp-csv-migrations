dad9b0229c3f7c54629c8473aaf245cf2d4ab069
George Constantinou <georgec20001@gmail.com>
2017-09-20 12:28:35 +0300

    Panels javascript optimization (task #812)
    Load panels js logic on document ready to avoid observe function
    triggering before form inputs are properly loaded, which generates
    multiple calls to the panels API endpoint.

--- webroot/js/panels.js
+++ webroot/js/panels.js
@@ -4,15 +4,18 @@
     var Panel = function (form) {
         this.form = form;
         this.url = $(form).data('panels-url');
-        if (!this.form || !this.url) {
+
+        if (!this.url) {
             return false;
         }
 
         if (!this.isEligible()) {
             return false;
         }
+
         //run an initial evaluation with current form's settings.
         this.evaluateWithServer();
+
         //Observe the form.
         this.observe();
     };
@@ -23,15 +26,12 @@
      * @return {Boolean} True if there is/are panel(s).
      */
     Panel.prototype.isEligible = function () {
-        var $form = this.form;
-
-        return $form.has('[data-provide="dynamic-panel"]').length ? true : false;
+        return $(this.form).has('[data-provide="dynamic-panel"]').length ? true : false;
     };
 
     Panel.prototype.buildData = function () {
-        var $form = this.form;
         var data = {};
-        $form.find(':input').each(function () {
+        $(this.form).find(':input').each(function () {
             var name = $(this).attr('name');
             var value = $(this).val();
             if (typeof name !== 'undefined' && typeof value !== 'undefined' ) {
@@ -43,14 +43,18 @@
     };
 
     Panel.prototype.hidePanels = function (panels) {
-        var $form = this.form;
-        if (typeof panels === 'undefined' || !panels instanceof Array) {
-            return false;
+        if (typeof panels === 'undefined') {
+            return;
         }
 
+        if (!(panels instanceof Array)) {
+            return;
+        }
+
+        var that = this;
         panels.forEach(function (cur) {
             var current = cur;
-            var $panel = $form.find('[data-provide="dynamic-panel"]');
+            var $panel = $(that.form).find('[data-provide="dynamic-panel"]');
             $panel.each(function () {
                 var title = $(this).find('[data-title="dynamic-panel-title"]').text();
                 if (current === title) {
@@ -64,22 +68,18 @@
     };
 
     Panel.prototype.resetPanels = function () {
-        var $form = this.form;
-        $form.find('[data-provide="dynamic-panel"].hidden').find(':input').attr('disabled', false);
-        $form.find('[data-provide="dynamic-panel"]').removeClass('hidden');
+        $(this.form).find('[data-provide="dynamic-panel"].hidden').find(':input').attr('disabled', false);
+        $(this.form).find('[data-provide="dynamic-panel"]').removeClass('hidden');
     };
 
     Panel.prototype.observe = function () {
-        var $form = this.form;
         var that = this;
-        $form.find(':input').change(function () {
+        $(this.form).find(':input').change(function () {
             that.evaluateWithServer();
         });
     };
 
     Panel.prototype.evaluateWithServer = function () {
-        var $form = this.form;
-        var token = api_options.token;
         var that = this;
         $.ajax({
             url: that.url,
@@ -87,20 +87,21 @@
             data: that.buildData(),
             dataType: 'json',
             headers: {
-                'Authorization': 'Bearer ' + token
+                'Authorization': 'Bearer ' + api_options.token
             },
             success: function (data) {
                 if (data.success) {
                     that.resetPanels();
-                    that.hidePanels(data.data['fail']);
+                    that.hidePanels(data.data.fail);
                 }
             },
         });
     };
 
-
-    $('form').each(function (i) {
-        new Panel($(this));
+    $(document).ready(function () {
+        $('form[data-panels-url]').each(function (i) {
+            new Panel(this);
+        });
     });
 
 })(jQuery);

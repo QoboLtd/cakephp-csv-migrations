8bed28e9bcd1fc4b488e91fa0bcbe8ba3d4b6a3e
George Constantinou <georgec20001@gmail.com>
2017-11-06 11:16:04 +0200

    Moved API logic to APP level (task #4795)

--- src/Event/EventName.php
+++ src/Event/EventName.php
@@ -18,20 +18,6 @@ use MyCLabs\Enum\Enum;
  */
 class EventName extends Enum
 {
-    // API controller events
-    const API_ADD_AFTER_SAVE = 'CsvMigrations.Add.afterSave';
-    const API_ADD_BEFORE_SAVE = 'CsvMigrations.Add.beforeSave';
-    const API_EDIT_AFTER_FIND = 'CsvMigrations.Edit.afterFind';
-    const API_EDIT_AFTER_SAVE = 'CsvMigrations.Edit.afterSave';
-    const API_EDIT_BEFORE_FIND = 'CsvMigrations.Edit.beforeFind';
-    const API_EDIT_BEFORE_SAVE = 'CsvMigrations.Edit.beforeSave';
-    const API_INDEX_AFTER_PAGINATE = 'CsvMigrations.Index.afterPaginate';
-    const API_INDEX_BEFORE_PAGINATE = 'CsvMigrations.Index.beforePaginate';
-    const API_INDEX_BEFORE_RENDER = 'CsvMigrations.Index.beforeRender';
-    const API_LOOKUP_AFTER_FIND = 'CsvMigrations.afterLookup';
-    const API_LOOKUP_BEFORE_FIND = 'CsvMigrations.beforeLookup';
-    const API_VIEW_AFTER_FIND = 'CsvMigrations.View.afterFind';
-    const API_VIEW_BEFORE_FIND = 'CsvMigrations.View.beforeFind';
     // Controller events
     const BATCH_IDS = 'CsvMigrations.Batch.ids';
     // Field Handlers events

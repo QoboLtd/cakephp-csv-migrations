7b3fccaf86bfb79d6bd5967ab39d08fe75edb1b9
George Constantinou <georgec20001@gmail.com>
2018-01-18 13:01:01 +0200

    Related plain Renderer (task #5219)
    Fixed the recursive handling of plain value rendering in related
    field handler.

---
+++ src/FieldHandlers/Provider/RenderValue/RelatedPlainRenderer.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace CsvMigrations\FieldHandlers\Provider\RenderValue;
+
+use CsvMigrations\FieldHandlers\RelatedFieldTrait;
+use InvalidArgumentException;
+
+/**
+ * RelatedPlainRenderer
+ *
+ * Related value as plain
+ */
+class RelatedPlainRenderer extends AbstractRenderer
+{
+    use RelatedFieldTrait;
+
+    /**
+     * Provide rendered value
+     *
+     * @param mixed $data Data to use for provision
+     * @param array $options Options to use for provision
+     * @return mixed
+     */
+    public function provide($data = null, array $options = [])
+    {
+        $result = null;
+
+        if (empty($data)) {
+            return $result;
+        }
+
+        if ('related' !== $options['fieldDefinitions']->getType()) {
+            return $data;
+        }
+
+        $relatedProperties[] = $this->_getRelatedProperties($options['fieldDefinitions']->getLimit(), $data);
+
+        if (!empty($relatedProperties[0]['config']['parent']['module'])) {
+            array_unshift(
+                $relatedProperties,
+                $this->_getRelatedParentProperties($relatedProperties[0])
+            );
+        }
+
+        $inputs = [];
+        foreach ($relatedProperties as $properties) {
+            if (empty($properties)) {
+                continue;
+            }
+
+            $inputs[] = $properties['dispFieldVal'];
+        }
+
+        if (!empty($inputs)) {
+            $result .= implode(' ' . $this->_separator . ' ', $inputs);
+        }
+
+        return $result;
+    }
+}

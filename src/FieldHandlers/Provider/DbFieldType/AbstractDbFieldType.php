e0f77b1182797d564f5e5722ec30cfe0da716846
Leonid Mamchenkov <leonid@mamchenkov.net>
2018-02-08 03:04:17 +0200

    Implemented DbFieldType providers (task #5061)
    Created provider classes that will substitute the `DB_FIELD_TYPE`
    constant used from within the field handler classes.

---
+++ src/FieldHandlers/Provider/DbFieldType/AbstractDbFieldType.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace CsvMigrations\FieldHandlers\Provider\DbFieldType;
+
+use CsvMigrations\FieldHandlers\Provider\AbstractProvider;
+
+/**
+ * AbstractDbFieldType
+ *
+ * Abstract DbFieldType provides the default functionality
+ * for fetching the field's database type.
+ */
+abstract class AbstractDbFieldType extends AbstractProvider
+{
+    /**
+     * @var string $dbFieldType Database field type
+     */
+    protected $dbFieldType = 'string';
+
+    /**
+     * Provide database field type
+     *
+     * @param mixed $data Data to use for provision
+     * @param array $options Options to use for provision
+     * @return string
+     */
+    public function provide($data = null, array $options = [])
+    {
+        return $this->dbFieldType;
+    }
+}
---
+++ src/FieldHandlers/Provider/DbFieldType/BlobDbFieldType.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace CsvMigrations\FieldHandlers\Provider\DbFieldType;
+
+/**
+ * BlobDbFieldType
+ *
+ * Blob database field type.
+ */
+class BlobDbFieldType extends AbstractDbFieldType
+{
+    /**
+     * @var string $dbFieldType Database field type
+     */
+    protected $dbFieldType = 'blob';
+}

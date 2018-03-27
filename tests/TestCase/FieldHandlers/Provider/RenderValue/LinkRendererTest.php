a2182196132f31451d442a13c3e19db7be6d9d6f
George Constantinou <georgec20001@gmail.com>
2018-03-27 11:42:05 +0300

    Added support for relative URLs that start with slash (task #5653)

--- tests/TestCase/FieldHandlers/Provider/RenderValue/LinkRendererTest.php
+++ tests/TestCase/FieldHandlers/Provider/RenderValue/LinkRendererTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace CsvMigrations\Test\TestCase\FieldHandlers\Provider\RenderValue;
 
+use Cake\Core\Configure;
 use CsvMigrations\FieldHandlers\Config\UrlConfig;
 use CsvMigrations\FieldHandlers\Provider\RenderValue\LinkRenderer;
 use PHPUnit_Framework_TestCase;
@@ -35,6 +36,7 @@ class LinkRendererTest extends PHPUnit_Framework_TestCase
             ['foobar', 'foobar', 'String'],
             ['2017-07-05', '2017-07-05', 'Date'],
             ['www.google.com', 'www.google.com', 'URL without schema'],
+            ['/foobar', '/foobar', 'Relative URL that starts with a slash'],
         ];
     }
 
@@ -65,5 +67,19 @@ class LinkRendererTest extends PHPUnit_Framework_TestCase
         $result = $this->renderer->provide('123&456', $options);
         $expected = '<a href="http://example.com/123%26456/" target="foobar">123&amp;456</a>';
         $this->assertEquals($expected, $result, "Value rendering is broken for encoded value with option");
+
+        $fullBaseUrl = Configure::read('App.fullBaseUrl');
+        // set custom full base url
+        Configure::write('App.fullBaseUrl', 'http://example.com');
+
+        // Relative
+        $this->assertEquals(
+            '<a href="http://example.com/123/" target="_blank">123</a>',
+            $this->renderer->provide(123, ['linkTo' => '/%s/']),
+            'Value rendering is broken for relative value with option'
+        );
+
+        // restore original full base url value
+        Configure::write('App.fullBaseUrl', $fullBaseUrl);
     }
 }

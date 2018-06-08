e9eaac6495621e6705cc4009e73189116593168e
George Constantinou <georgec20001@gmail.com>
2018-06-08 15:25:16 +0300

    Adjusted styling and layout of files value rendering (task #6191)

--- src/FieldHandlers/Provider/RenderValue/FilesRenderer.php
+++ src/FieldHandlers/Provider/RenderValue/FilesRenderer.php
@@ -21,46 +21,6 @@ use CsvMigrations\Utility\FileUpload;
 class FilesRenderer extends AbstractRenderer
 {
     /**
-     * CSS Framework grid columns number
-     */
-    const GRID_COUNT = 12;
-
-    /**
-     * Limit of thumbnails to display
-     */
-    const THUMBNAIL_LIMIT = 6;
-
-    /**
-     * Default thumbnail file
-     */
-    const NO_THUMBNAIL_FILE = 'no-thumbnail.jpg';
-
-    /**
-     * Thumbnail html markup
-     */
-    const THUMBNAIL_HTML = '<div class="thumbnail">%s</div>';
-
-    /**
-     * CSS Framework row html markup
-     */
-    const GRID_ROW_HTML = '<div class="row">%s</div>';
-
-    /**
-     * CSS Framework row html markup
-     */
-    const GRID_COL_HTML = '<div class="col-xs-%d col-sm-%d col-md-%d col-lg-%d">%s</div>';
-
-    /**
-     * Icon extension
-     */
-    const ICON_EXTENSION = 'png';
-
-    /**
-     * Icon size
-     */
-    const ICON_SIZE = '48';
-
-    /**
      * Provide rendered value
      *
      * @param mixed $data Data to use for provision
@@ -82,76 +42,13 @@ class FilesRenderer extends AbstractRenderer
 
         $entities = $fileUpload->getFiles($table, $field, $data);
 
-        if (!empty($entities)) {
-            $result = $this->_filesHtml($entities, $fileUpload);
-        }
+        $params = [
+            'entities' => $entities
+        ];
 
-        return $result;
-    }
+        $defaultElement = 'CsvMigrations.FieldHandlers/FilesFieldHandler/value';
+        $element = empty($options['element']) ? $defaultElement : $options['element'];
 
-    /**
-     * Generates file icons markup
-     *
-     * @param \Cake\ORM\ResultSet $entities File Entities
-     * @param \CsvMigrations\Utility\FileUpload $fileUpload FileUpload instance
-     *
-     * @return string
-     */
-    protected function _filesHtml($entities, FileUpload $fileUpload)
-    {
-        $result = null;
-        $colWidth = static::GRID_COUNT / static::THUMBNAIL_LIMIT;
-        $thumbnailUrl = 'CsvMigrations.thumbnails/' . static::NO_THUMBNAIL_FILE;
-
-        $view = $this->config->getView();
-        foreach ($entities as $k => $entity) {
-            if ($k >= static::THUMBNAIL_LIMIT) {
-                break;
-            }
-
-            $thumbnailUrl = $this->_getFileIconUrl($entity->extension);
-            $thumbnail = sprintf(
-                static::THUMBNAIL_HTML,
-                $view->Html->image($thumbnailUrl, ['title' => $entity->filename])
-            );
-
-            $thumbnail = $view->Html->link($thumbnail, $entity->path, ['escape' => false, 'target' => '_blank']);
-
-            $result .= sprintf(
-                static::GRID_COL_HTML,
-                $colWidth,
-                $colWidth,
-                $colWidth,
-                $colWidth,
-                $thumbnail
-            );
-        }
-
-        $result = sprintf(static::GRID_ROW_HTML, $result);
-
-        return $result;
-    }
-
-    /**
-     * Get file icon url by file extension
-     *
-     * @param  string $extension File extension
-     * @return string
-     */
-    protected function _getFileIconUrl($extension)
-    {
-        $file = strtolower($extension);
-        $webroot = dirname(__FILE__) . DS . '..' . DS . '..' . DS . 'webroot' . DS;
-        $filesDir = $webroot . 'img' . DS . 'icons' . DS . 'files' . DS . '48px' . DS;
-
-        if (!file_exists($filesDir . $file . '.' . static::ICON_EXTENSION)) {
-            $file = '_blank';
-        }
-
-        $view = $this->config->getView();
-
-        return $view->Url->image(
-            'CsvMigrations.icons/files/' . static::ICON_SIZE . 'px/' . $file . '.' . static::ICON_EXTENSION
-        );
+        return $this->renderElement($element, $params);
     }
 }
---
+++ src/Template/Element/FieldHandlers/FilesFieldHandler/value.ctp
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+use Cake\Core\Plugin;
+
+$uuid = $this->Text->uuid();
+?>
+<div class="row">
+<?php foreach ($entities as $index => $entity) : ?>
+    <?php if (3 < $entities->count() && 3 === $index) : ?>
+        <div class="col-xs-12">
+            <p class="text-right">
+                <a href="#collapseFiles<?= $uuid ?>" class="btn btn-default btn-sm" data-toggle="collapse" aria-controls="collapseFiles<?= $uuid ?>" aria-expanded="false" role="button">
+                    <span class="dropdown">
+                        <span class="caret"></span>
+                    </span>
+                </a>
+            </p>
+        </div>
+        <div class="collapse" id="collapseFiles<?= $uuid ?>">
+    <?php endif; ?>
+    <div class="col-xs-4">
+        <a href="<?= $entity->get('path') ?>" target="_blank">
+            <div class="thumbnail" title="<?= $entity->get('filename') ?>">
+                <?php
+                $path = Plugin::path('CsvMigrations') . DS . 'webroot' . DS . 'img' . DS . 'icons' . DS;
+                $path .= 'files' . DS . '48px' . DS . strtolower($entity->get('extension')) . '.png';
+                $filename = file_exists($path) ? strtolower($entity->get('extension')) : '_blank';
+                ?>
+                <?= $this->Html->image('CsvMigrations.icons/files/48px/' . $filename . '.png') ?>
+                <div class="caption">
+                    <p class="small text-center no-margin" style="white-space: nowrap; text-overflow: ellipsis;overflow: hidden;">
+                        <?= $entity->get('filename') ?>
+                    </p>
+                </div>
+            </div>
+        </a>
+    </div>
+<?php endforeach; ?>
+<?php if (3 < $entities->count()) : ?>
+    </div>
+<?php endif; ?>
+</div>
+<?php if (3 < $entities->count()) {
+    echo $this->Html->scriptBlock(
+        '$("a[href=\'#collapseFiles' . $uuid . '\']").click(function () {
+            $(this).children().hasClass("dropdown") ?
+                $(this).children().removeClass("dropdown").addClass("dropup") :
+                $(this).children().removeClass("dropup").addClass("dropdown");
+        });',
+        ['block' => 'scriptBottom']
+    );
+}
+?>

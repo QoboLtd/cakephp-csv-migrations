68e7c89ec51c8ce6070b95a0bb08ecbe1200b1bd
George Constantinou <georgec20001@gmail.com>
2018-09-13 16:08:42 +0300

    Descriptive comment for control structure (task #7147)

--- src/Utility/Validate/Check/ViewsCheck.php
+++ src/Utility/Validate/Check/ViewsCheck.php
@@ -66,7 +66,7 @@ class ViewsCheck extends AbstractCheck
             }
 
             foreach ($fields as $field) {
-                if (count($field) > 13) {
+                if (count($field) > 13) { // Panel name + 12 fields of the grid maximum
                     $this->errors[] = $module . " module [$view] view has more than 12 columns";
                     continue;
                 }

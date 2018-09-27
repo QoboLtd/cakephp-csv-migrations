e3d36eb19a3f304cc22f95dd94da9dd23765e165
George Constantinou <georgec20001@gmail.com>
2018-09-27 14:51:36 +0300

    Aggregator interface (task #6030)

---
+++ src/Aggregator/AggregatorInterface.php
@@ -0,0 +1,31 @@
+<?php
+namespace CsvMigrations\Aggregator;
+
+use Cake\Datasource\EntityInterface;
+use Cake\Datasource\QueryInterface;
+
+interface AggregatorInterface
+{
+    /**
+     * Validates aggregator config.
+     *
+     * @return bool
+     */
+    public function validate();
+
+    /**
+     * Applies aggregation conditions.
+     *
+     * @param \Cake\Datasource\QueryInterface $query Query instance
+     * @return \Cake\Datasource\QueryInterface
+     */
+    public function applyConditions(QueryInterface $query);
+
+    /**
+     * Returns aggregated result.
+     *
+     * @param \Cake\Datasource\ResultSetInterface $resultSet ResultSet instance
+     * @return string
+     */
+    public function getResult(EntityInterface $resultSet);
+}

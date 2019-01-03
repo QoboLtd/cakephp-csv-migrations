e8e73848cb50dbf12373ee2e920a1ffe435d503a
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-01-03 15:20:52 +0200

    Fixing AdminLTE paths (task #9313)

--- src/FieldHandlers/Provider/SearchOptions/CountrySearchOptions.php
+++ src/FieldHandlers/Provider/SearchOptions/CountrySearchOptions.php
@@ -66,7 +66,7 @@ class CountrySearchOptions extends AbstractSearchOptions
                     'type' => 'script',
                     'content' => [
                         'CsvMigrations.dom-observer',
-                        'AdminLTE./plugins/select2/select2.full.min',
+                        'AdminLTE./bower_components/select2/dist/js/select2.full.min',
                         'CsvMigrations.select2.init'
                     ],
                     'block' => 'scriptBottom'
@@ -74,7 +74,7 @@ class CountrySearchOptions extends AbstractSearchOptions
                 [
                     'type' => 'css',
                     'content' => [
-                        'AdminLTE./plugins/select2/select2.min',
+                        'AdminLTE./bower_components/select2/dist/css/select2.min',
                         'Qobo/Utils.select2-bootstrap.min',
                         'Qobo/Utils.select2-style',
                         'Qobo/Utils./img/icons/flags/css/flag-icon.css'

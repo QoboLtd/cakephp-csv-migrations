6a69881472abe3eb9de2eefaad6a000a2be0630e
Demetris Papademetriou <d.papademetrioug13@gmail.com>
2019-01-15 16:40:08 +0200

    Added a new commonmark render handler(task #2669)

---
+++ src/FieldHandlers/Config/CommonMarkConfig.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Qobo Ltd. (https://www.qobo.biz)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace CsvMigrations\FieldHandlers\Config;
+
+/**
+ * CommonMarkConfig
+ *
+ * This class provides the predefined configuration
+ * for text field handlers.
+ */
+class CommonMarkConfig extends FixedConfig
+{
+    /**
+     * @var array $providers List of provider names and classes
+     */
+    protected $providers = [
+        'combinedFields' => '\\CsvMigrations\\FieldHandlers\\Provider\\CombinedFields\\NullCombinedFields',
+        'dbFieldType' => '\\CsvMigrations\\FieldHandlers\\Provider\\DbFieldType\\TextDbFieldType',
+        'fieldValue' => '\\CsvMigrations\\FieldHandlers\\Provider\\FieldValue\\MixedFieldValue',
+        'fieldToDb' => '\\CsvMigrations\\FieldHandlers\\Provider\\FieldToDb\\TextFieldToDb',
+        'searchOperators' => '\\CsvMigrations\\FieldHandlers\\Provider\\SearchOperators\\StringSearchOperators',
+        'searchOptions' => '\\CsvMigrations\\FieldHandlers\\Provider\\SearchOptions\\StringSearchOptions',
+        'selectOptions' => '\\CsvMigrations\\FieldHandlers\\Provider\\SelectOptions\\NullSelectOptions',
+        'renderInput' => '\\CsvMigrations\\FieldHandlers\\Provider\\RenderInput\\TextRenderer',
+        'renderValue' => '\\CsvMigrations\\FieldHandlers\\Provider\\RenderValue\\CommonMarkRenderer',
+        'renderName' => '\\CsvMigrations\\FieldHandlers\\Provider\\RenderName\\DefaultRenderer',
+        'validationRules' => '\\CsvMigrations\\FieldHandlers\\Provider\\ValidationRules\\TextValidationRules',
+    ];
+}

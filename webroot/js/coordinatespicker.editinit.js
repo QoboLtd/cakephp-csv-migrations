cf1e2f747db68c63eb9035a3305614882408f065
George Constantinou <georgec20001@gmail.com>
2020-02-06 12:11:47 +0200

    Check value length only if value is defined (task #15152)

--- webroot/js/coordinatespicker.editinit.js
+++ webroot/js/coordinatespicker.editinit.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     $('.coordinates_modal').each(function (event) {
         let gps_input = $(this).prev().find('input')
-        let gps_value = $(gps_input).attr("value").length > 0 ? $(gps_input).attr("value") : $(gps_input).attr("default_coordinates")
+        let gps_value = $(gps_input).attr("value") && $(gps_input).attr("value").length > 0 ?
+            $(gps_input).attr("value") :
+            $(gps_input).attr("default_coordinates")
         $(this).on('shown.bs.modal', function () {
             $(this).find(".modal-body").append('<div style="height:300px" id="map"></div>')
             drawMap("map", gps_value)
